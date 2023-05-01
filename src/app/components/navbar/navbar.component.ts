4f28ac0a014f03719188fce4fe496e912397c656
hugopaul <hugo.4500@icloud.com>
2023-05-01 17:03:59 -0300

    first commit

---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,11 @@
+import { Component, ElementRef } from '@angular/core';
+import { ROUTES } from '../sidebar/sidebar.component';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss']
+})
+export class NavbarComponent {
+}
