0c7870c3b0d5bbe4174bdaf3abbe9eb409a07f1a
hugopaul <hugo.4500@icloud.com>
2023-05-07 10:04:00 -0300

    lancamentos

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://solidtechsolutions.com.br:8081/api'
+    apiUrl: 'https://solidtechsolutions.com.br:8081/api'
   };
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://solidtechsolutions.com.br:8082/api'
+    apiUrl: 'https://solidtechsolutions.com.br:8082/api'
   };
